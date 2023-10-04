e99ac4e5e4a2c21221cf4c77cdb26c3275dbabc3
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-10-04 13:31:22 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+//import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@myst-theme/providers";
+import { MyST, DEFAULT_RENDERERS } from "myst-to-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {fromMarkdown} from 'mdast-util-from-markdown'
+
+const SignatureRenderer = ({ node }) => {
+  return (
+    <>
+      <div className="flex my-5 group">
+        <div className="flex-grow overflow-x-auto overflow-y-hidden" />
+        {node.value}
+      </div>
+      <div>
+        <MyST ast={node.children} />
+      </div>
+    </>
+  );
+};
+
+const LOC = { signature: SignatureRenderer };
+const RENDERERS = { ...DEFAULT_RENDERERS, ...LOC };
+
+function MyComponent({ node }) {
+  console.log("Node", node);
+  return (
+    <ThemeProvider renderers={RENDERERS}>
+      <MyST ast={node.children} />
+    </ThemeProvider>
+  );
+}
+
+const tree = fromMarkdown('Some *emphasis*, **strong**, and `code`.')
+const mytree = {
+  type: 'admonition',
+  children: [
+    { type: "text", value: "myValue" },
+    {
+      type: "signature",
+      value: "Foo",
+      children: [{ type: "text", value: "Child" }],
+    },
+  ],
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyComponent node={tree} />
   </React.StrictMode>
 );
 
